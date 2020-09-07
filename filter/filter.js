21b9d2a3dfdea0a20828113c5acbee341ad33894
DmitryAstrashenia <Dimon713@tut.by>
2020-09-07 22:33:51 +0300

    07.09.2020

--- filter/filter.js
+++ filter/filter.js
@@ -4,48 +4,30 @@ let Filter = React.createClass({
   getInitialState: function () {
     return {
       dataBase: this.props.dataBase,
-      oldDataBase: this.props.dataBase,
-      defaultValue: "",
+      inputValue: "",
       isChecked: false,
     };
   },
 
-  filter: function (e) {
+  filter: function () {
     let newDataBase = [];
-    this.props.dataBase.forEach((el) => {
-      str = el.text;
-      if (str.match(e.target.value)) {
-        newDataBase.push(el);
-      }
-    });
-    this.setState({
-      dataBase: newDataBase,
-      defaultValue: e.target.value,
-    });
-    console.log("тыц");
-  },
-
-  refreash: function () {
-    this.setState({
-      dataBase: this.props.dataBase,
-      defaultValue: "",
-      isChecked: false,
-    });
-  },
-
-  triger: function (e) {
-    if (e.target.checked) {
-      this.state.isChecked = true;
-      this.SortABC();
+    if (this.state.inputValue) {
+      this.props.dataBase.forEach((el) => {
+        str = el.text;
+        if (str.match(this.state.inputValue)) {
+          newDataBase.push(el);
+        }
+      });
+      this.setState({
+        dataBase: newDataBase,
+      });
     } else {
-      this.state.isChecked = false;
-      this.SortABC();
+      this.setState({
+        dataBase: this.props.dataBase,
+      });
     }
-  },
 
-  SortABC: function () {
     if (this.state.isChecked) {
-      this.state.oldDataBase = this.state.dataBase;
       let myarray = [];
       this.state.dataBase.forEach((el) => {
         myarray.push(el.text);
@@ -61,28 +43,53 @@ let Filter = React.createClass({
       });
       this.setState({
         dataBase: ABCdataBase,
-        isChecked: true,
-      });
-    } else {
-      this.setState({
-        dataBase: this.state.oldDataBase,
-        isChecked: false,
       });
     }
+  },
+
+  refreash: function () {
+    this.setState(
+      {
+        dataBase: this.props.dataBase,
+        inputValue: "",
+        isChecked: false,
+      },
+      this.filter
+    );
+  },
+
+  checkOnOf: function (e) {
+    this.setState(
+      {
+        isChecked: e.target.checked,
+      },
+      this.filter
+    );
+  },
+
+  inputChange: function (e) {
+    this.setState(
+      {
+        inputValue: e.target.value,
+      },
+      this.filter
+    );
   },
 
   render: function () {
     let dataBaseCodeOption = [];
     this.state.dataBase.forEach((el) => {
       dataBaseCodeOption.push(
-        React.DOM.option({
+        React.DOM.option(
+          {
             key: `${el.code}`,
           },
           `${el.text}`
         )
       );
     });
-    let dataBaseCode = React.DOM.select({
+    let dataBaseCode = React.DOM.select(
+      {
         className: "list",
         name: "list",
         size: "5",
@@ -90,18 +97,19 @@ let Filter = React.createClass({
       dataBaseCodeOption
     );
 
-    return React.DOM.div({
+    return React.DOM.div(
+      {
         className: "Filter",
       },
       React.DOM.input({
         type: "checkbox",
-        onClick: this.triger,
         checked: this.state.isChecked,
+        onChange: this.checkOnOf,
       }),
       React.DOM.input({
         type: "text",
-        onChange: this.filter,
-        value: this.state.defaultValue,
+        value: this.state.inputValue,
+        onChange: this.inputChange,
       }),
 
       React.DOM.input({
